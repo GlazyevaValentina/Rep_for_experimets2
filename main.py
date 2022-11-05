8e461917255f6129540031305b019c79cb6ddddb
GlazyevaValentina <vglazyeva@gmail.com>
2022-11-05 16:41:24 +0200

    Commit 5. Counting kittens and pupies test

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def describe_pet(animal_type, pet_name):
 # describe_pet("cat", "vasya")
 describe_pet("cat", "semen")
 print("I love my pet")
+# Transition to a branch test
+def test_count_quantity(kittens, puppies):
+    quantity = kittens + puppies
+    print(quantity)
+
+test_count_quantity(3,3)
