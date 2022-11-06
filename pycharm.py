565d9789ea412ed2c81f2f200add64f5baf9b1c2
GlazyevaValentina <vglazyeva@gmail.com>
2022-11-06 11:43:13 +0200

    Commit 1. Creating file with one function

--- pycharm.py
+++ pycharm.py
@@ -1,10 +1,6 @@
-
 def describe_pet(animal_type, pet_name):
     # Information about pet_name
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-# describe_pet("cat", "vasya")
-describe_pet("cat", "semen")
-print("I love my pet")
-print("Whatever happens,\nall my pets are always with me.")
+describe_pet("cat", "vasya")
