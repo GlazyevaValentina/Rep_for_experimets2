3a4f1dcbd33b38971d2e1f5c541b20edb7f180b7
GlazyevaValentina <vglazyeva@gmail.com>
2022-11-06 09:28:30 +0200

    Commit 1. Creating file with one function Commit 2. The same function new argument Commit 3. Error correction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def describe_pet(animal_type, pet_name):
     # Information about pet_name
     print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s is {pet_name.title()}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
     
 # describe_pet("cat", "vasya")
 describe_pet("cat", "semen")
